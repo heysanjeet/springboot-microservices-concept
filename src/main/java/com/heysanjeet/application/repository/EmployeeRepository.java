4e4b9106865c8c9f524ce1483af53939896578c1
sanjeetkumar <imsanjeetkr7@gmail.com>
2025-08-03 14:30:34 +0530

    initial commit

--- src/main/java/com/heysanjeet/application/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.heysanjeet.application.repository;
-import com.heysanjeet.application.domain.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends MongoRepository<Employee,Integer> {
-}
